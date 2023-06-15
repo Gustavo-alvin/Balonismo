56ad4f6538544daddf0cd48fdf4a348ae3f53213
Amanda Seabra <amandaseabra47@gmail.com>
2023-06-15 11:52:07 -0300

    Initial commit

---
+++ Balonismo/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        //criar objetos do passeio
+        Pessoa p1 = new Pessoa();
+        Pessoa p2; //DECLARAÇÃO DO OBJETO
+        p2 = new Pessoa();
+        //...
+        p1.nome = "Elliot Page";
+
+    }
+}
